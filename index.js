91eb9ac32485ee29498649ac1512f41bf7e13774
李成 <hello1024lc@gmail.com>
2021-01-29 21:39:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,27 +18,27 @@ const emiter = new Emit();
 
 const tasks = [
     {
-        task: () => console.log(1),
+        task: _ => _,
         after: false,
         duration: 2
     },
     {
-        task: () => console.log(2),
+        task: _ => _,
         after: true,
         duration: 2
     },
     {
-        task: () => console.log(3),
+        task: _ => _,
         after: false,
         duration: 20
     },
     {
-        task: () => console.log(4),
+        task: _ => _,
         after: true,
         duration: 2
     },
     {
-        task: () => console.log(5),
+        task: _ => _,
         after: true,
         duration: 2
     }
@@ -47,15 +47,19 @@ const tasks = [
 const initialTask = () => Promise.resolve();
 
 const compose = (list = []) => {
-    if (!list.length) return _ => _;
-    return list.reduceRight((pre, item, index) => {
+    if (!list.length) return initialTask;
+    return list.reduce((pre, item, index) => {
         const { task, after, duration } = item;
 
         return () => new Promise((resolve, reject) => {
             emiter.on('over' + index, resolve); // 收到事件提前结束
 
             const runner = () => {
-                setTimeout(resolve, duration * 1000);
+                setTimeout(() => {
+                    resolve();
+                    console.log('task' + index + 'end');
+                }, duration * 1000);
+                console.log('task' + index + 'start');
                 task();
             };
 
